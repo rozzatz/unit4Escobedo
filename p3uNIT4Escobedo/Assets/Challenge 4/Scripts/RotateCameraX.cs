596c83e873170cb460b1c7ab99b35a426be82419
bruce <145066600+rozzatz@users.noreply.github.com>
2024-09-10 11:46:24 -0700

    almost finished challenge except the bonus

---
+++ p3uNIT4Escobedo/Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCameraX : MonoBehaviour
+{
+    private float Speed = 200;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.Rotate(Vector3.up, Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            transform.Rotate(Vector3.up, -Speed * Time.deltaTime);
+        } 
+
+        transform.position = player.transform.position; // Move focal point with player
+
+    }
+}
